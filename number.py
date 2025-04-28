091c905babcec70bebdb88ee83dc3d76086b52eb
Hanjala <hanjalashihab1@gmail.com>
2025-04-28 19:28:27 +0600

    numbers

---
+++ number.py
@@ -0,0 +1,64 @@
+x = 2 + 3
+print(x)
+
+
+y = 5 - 2
+print(y)
+
+
+z = 3 * 4
+print(z)
+
+a = 10 / 2
+print(a)
+
+
+
+#To find the remainder of a division:
+b = 10 % 3
+print(b)
+
+
+#To find the power of a number:
+c = 2 ** 3
+print(c)
+
+
+#Using round function:
+d = 5.6789
+print(round(d, 2))  # Round to 2 decimal places
+
+
+length = 125.65
+width = 74.45
+
+area = length * width
+print("Area of the rectangle:", area)
+print(round(area, 2))
+
+
+#Using abs function:
+e = -10
+print(abs(e))  # Absolute value
+
+
+#Using min and max functions:
+f = min(5, 10, 15)
+g = max(5, 10, 15)
+print(f)  # Minimum value
+print(g)  # Maximum value
+
+
+#Using pow function:
+h = pow(2, 3)  # 2 raised to the power of 3
+print(h)  # 8
+
+#Using divmod function:
+i = divmod(10, 3)  # Returns quotient and remainder
+print(i)  # (3, 1) - 3 is the quotient and 1 is the remainder
+
+#Using sum function:
+j = sum([1, 2, 3, 4, 5])  # Sum of a list
+print(j)
+
+
