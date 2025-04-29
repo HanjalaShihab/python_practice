e97918cdd078dd5adcd8a2b1be8cfd865b700e11
Hanjala <hanjalashihab1@gmail.com>
2025-04-29 15:18:52 +0600

    Changing list item values

---
+++ Lists/changeItems.py
@@ -0,0 +1,15 @@
+items = ["Canada", "USA", "Bangladesh", "Pakistan", "Germany"]
+items[3] = "Tukey"
+print(items)
+
+
+#Changing a range of items:
+items[1:3] = ["Bhutan", "Nepal"]
+print(items)
+
+
+
+
+items = ["Canada", "USA", "Bangladesh", "Pakistan", "Germany"]
+items[1:2] = ["Afghanistan", "Turkey"]
+print(items)
