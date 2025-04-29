de841f30f431595bb7d714f1f36849f7e305f6ba
Hanjala <hanjalashihab1@gmail.com>
2025-04-29 14:42:04 +0600

    basic list

---
+++ Lists/basic.py
@@ -0,0 +1,30 @@
+#List is a collection which is ordered and changeable. Allows duplicate members.
+
+#Creating a list:
+myList = ["apple", "banana", "cherry"]
+print(myList)
+
+
+
+#list allows duplicate values:
+newList = ['shihab', 'hanjala', 'shihab', 'muhammad']
+print(newList)
+
+
+
+#len of a list:
+myList = [1, 2, 3, 4, 5]
+print(len(myList))
+myList2 = ['shihab', 'hanjala', 'muhammad']
+print(len(myList2))
+
+
+
+#list allows different data types:
+items = ["apple", 1,2, True, 3.1416]
+print(items)
+
+
+
+#type of a list:
+print(type(items))
