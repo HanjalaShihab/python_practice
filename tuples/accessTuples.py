5e7b703a1dcf4cdc0e29648e8a2d3c1802c3896f
Hanjala <hanjalashihab1@gmail.com>
2025-05-04 00:16:29 +0600

    access tuples

--- tuples/accessTuples.py
+++ tuples/accessTuples.py
@@ -1,2 +1,20 @@
 thisTuple = (1, 2, 3, 4, 5)
 print(thisTuple[1])
+
+
+thisTuple = ("apple", "banana", "cherry","apple", "banana", "cherry")
+print(thisTuple[3:5])  #excluding 5th index
+
+
+thisTuple = ("apple", "banana", "cherry")
+print(thisTuple[-1])  #last item
+
+
+thisTuple = ("apple", "banana", "cherry")
+print(thisTuple[-2])  #second last item
+
+
+
+thisTuple = ("apple", "banana", "cherry")
+if "apple" in thisTuple:
+    print("There is an apple in the tuple")
