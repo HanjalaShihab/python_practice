5b5bc0fec8f02c4531f2bc4008a9f8086ef4bec3
Hanjala <hanjalashihab1@gmail.com>
2025-05-04 20:43:26 +0600

    access a set

---
+++ sets/accessSets.py
@@ -0,0 +1,10 @@
+thisSet = {"amar", "name", "Hanjala", "Shihab"}
+for x in thisSet:
+    print(x)
+    
+
+
+if "Shihab" in thisSet:
+    print("Yes, Shihab is here!")
+#short form:
+print("Shihab" in thisSet)
