b69c789f70a0df89da16b7cfe040df5257725699
Hanjala <hanjalashihab1@gmail.com>
2025-05-08 14:04:35 +0600

    change dictionary items

---
+++ dictionaries/changeItem.py
@@ -0,0 +1,17 @@
+dictionary = {
+    "first": "kichuna",
+    "second": "abar ki",
+    "third": "kichuina chill"
+}
+dictionary["third"] = "bolbo nah"
+
+print(dictionary)
+
+
+#using the update() method:
+
+dictionary.update({"third": "abar jiggasha korle mair dibo"})
+
+print(dictionary)
+
+print(dictionary.values())
