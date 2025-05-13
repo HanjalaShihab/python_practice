e23fae93d851f2781d1610299d5b9bd0c3a4ede3
Hanjala <hanjalashihab1@gmail.com>
2025-05-13 11:40:39 +0600

    modules in python

--- modules.py
+++ modules.py
@@ -1,6 +1,7 @@
 #modules is like importing another python file to an another file
 #it can be built in or any common file we develop
 #we can import partially anything from a file or the whole file
+
 #importing 'platform' which is a built-in module.
 import platform
 
