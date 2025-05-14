94fc1e9b0af4e56b405e9591ac2c518678750775
Hanjala <hanjalashihab1@gmail.com>
2025-05-14 12:31:03 +0600

    file open

---
+++ Files/fileOpen.py
@@ -0,0 +1,7 @@
+f = open("demofile.txt")
+
+#the code above is the same as:
+f = open("demofile.txt", "rt")
+
+
+#in that case of the above code, there is no file existing name demofile.txt.So a fileerror will be thrown.
